0371f54f584ee0713e0485c3e2c9812f05c08589
gabrielramalhao <gabriel123.ramalhao@gmail.com>
2025-08-08 07:41:04 -0300

    feat: gemini option (working)

--- assistente-ia/script.js
+++ assistente-ia/script.js
@@ -6,12 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
   chatForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const userMessage = userInput.value.trim();
-    const responseFromApi = getResponseFromApi(userMessage);
 
-    if (responseFromApi) {
-      addMessage(responseFromApi, "user");
+    if (userMessage) {
+      addMessage(userMessage, "user");
+      getResponseFromApi(userMessage);
       userInput.value = "";
-      processUserMessage(responseFromApi);
     }
   });
 
@@ -23,43 +22,27 @@ document.addEventListener("DOMContentLoaded", () => {
     chatBox.scrollTop = chatBox.scrollHeight;
   }
 
-  function processUserMessage(message) {
-    const lowerCaseMessage = message.toLowerCase();
-    let botResponse = "";
-
-    if (lowerCaseMessage.includes("olá") || lowerCaseMessage.includes("oi")) {
-      botResponse = "Olá! Como posso ajudar você hoje?";
-    }
-
-  }
-
   async function getResponseFromApi(message) {
-    const apiKey = document.getElementById("api-key").value;
+    const apiKey = document.getElementById("api-key").value.trim();
 
     if(!apiKey) {
       alert("Chave inválida");
       return;
     }
 
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: message }],
+        contents: [{ parts: [{ text: message }] }],
       }),
     });
 
     const data = await response.json();
 
-    if(data.choices && data.choices.length > 0) {
-      const botResponse = data.choices[0].message.content.trim();
-      return botResponse;
-    } else {
-      addMessage("Erro ao obter resposta", "bot");
-    }
+    return addMessage(data["candidates"][0]["content"]["parts"][0]["text"], "bot");
   }
 
   addMessage("Olá! Sou seu Assistente de IA. Como posso ajudar?", "bot");
